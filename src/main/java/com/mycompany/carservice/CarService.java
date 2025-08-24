a184b61c97cf332b8062b4485bd264429a50e3a7
XXI <152409931+NannapatXXI@users.noreply.github.com>
2025-08-24 18:05:57 +0700

    Update CarService.java

--- src/main/java/com/mycompany/carservice/CarService.java
+++ src/main/java/com/mycompany/carservice/CarService.java
@@ -5,8 +5,7 @@
 package com.mycompany.carservice;
 
 import com.mycompany.carservice.gui.MainPage;
-import com.mycompany.carservice.dao.DBConnection;
-import java.sql.Connection;
+
 /**
  *
  * @author nannapat
@@ -17,11 +16,6 @@ public class CarService {
         System.out.println("Hello World!");
         new MainPage();
         
-        //โหลดไฟล์ไปแล้วคอมเม้นบรรทัดพวกนี้ทิ้งด้วยเพราะเชื่อม database ไว้
-         try (Connection conn = DBConnection.getConnection()) {
-            System.out.println("✅ Connected to MySQL successfully!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+       
     }
 }
