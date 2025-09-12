61156f8d1f33a8e9983d75e0b6f8791000ee50d3
XXI <152409931+NannapatXXI@users.noreply.github.com>
2025-09-12 19:10:23 +0700

    v0.7

--- src/main/java/com/mycompany/carservice/gui/PopInBooking.java
+++ src/main/java/com/mycompany/carservice/gui/PopInBooking.java
@@ -4,36 +4,21 @@
  */
 package com.mycompany.carservice.gui;
 
-import com.mycompany.carservice.entity.CSVHandler;
-import java.util.List;
-import java.util.ArrayList;
-
 /**
  *
  * @author nannapat
  */
-public class PopInBooking extends javax.swing.JDialog {
+public class popInBooking extends javax.swing.JDialog {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(PopInBooking.class.getName());
-    private String name;
-     private String userId;
-    private ArrayList<String[]> bookingDetail = new ArrayList<>();
-     ArrayList<String[]> users ;
-     
-    CSVHandler csvHandler ;
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(popInBooking.class.getName());
+
     /**
-     * Creates new form popInBooking   
+     * Creates new form popInBooking
      */
-    public PopInBooking(java.awt.Frame parent, boolean modal,String name) {// java.awt.Frame parent เอาไว้ให้ class นี้รู้ว่าจะต้อง pop หน้าจอตัวเอาที่ใคร
+    public popInBooking(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
-        this.name = name;
-         csvHandler = new CSVHandler("src/main/data/user.csv");
-       
         initComponents();
         setLocationRelativeTo(null);
-        this.users = csvHandler.readCSV();
-
-         cheackuser();
         
     }
     public void setDayLabel(String text) {
@@ -47,30 +32,6 @@ public class PopInBooking extends javax.swing.JDialog {
     public void setServiceLabel(String text) {
      serviceLabel.setText(text);
     }
-    public void cheackuser(){
-       
-       for(String[] data : users){
-        if(data[1].trim().equalsIgnoreCase(name.trim())){
-            System.out.println("User found: " + String.join(", ", data));
-            userLabel.setText(data[1]);
-            break;
-        }
-    }
-      
-      
-    }
-    private void saveBooking(String[] newBooking) {
-    // 1. อ่านข้อมูลเก่าจาก CSV
-    csvHandler = new CSVHandler("src/main/data/history_user.csv");
-       
-    ArrayList<String[]> allBookings = csvHandler.readCSV();
-
-    // 2. เพิ่มข้อมูลใหม่
-    allBookings.add(newBooking);
-
-    // 3. เขียนทั้งหมดกลับไปไฟล์
-    csvHandler.writeCSV(allBookings);
-}
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -82,16 +43,12 @@ public class PopInBooking extends javax.swing.JDialog {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        textFieldCar = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
         dayLabel = new javax.swing.JLabel();
         timeLabel1 = new javax.swing.JLabel();
         serviceLabel = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        textDetail = new javax.swing.JTextArea();
-        backBtn = new javax.swing.JButton();
-        jLabel7 = new javax.swing.JLabel();
-        userLabel = new javax.swing.JLabel();
-        okBtn = new javax.swing.JButton();
+        jTextArea1 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -125,71 +82,40 @@ public class PopInBooking extends javax.swing.JDialog {
         serviceLabel.setForeground(new java.awt.Color(255, 255, 255));
         serviceLabel.setText("....");
 
-        textDetail.setColumns(20);
-        textDetail.setRows(5);
-        jScrollPane1.setViewportView(textDetail);
-
-        backBtn.setText("Back");
-        backBtn.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                backBtnMouseClicked(evt);
-            }
-        });
-
-        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel7.setText("User :");
-
-        userLabel.setForeground(new java.awt.Color(255, 255, 255));
-        userLabel.setText("....");
-
-        okBtn.setText("Ok");
-        okBtn.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                okBtnMouseClicked(evt);
-            }
-        });
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(156, 156, 156)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(83, 83, 83)
+                .addGap(70, 70, 70)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel4)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel2)
+                        .addComponent(jLabel3))
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel6)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(backBtn)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(okBtn))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jLabel4)
-                                .addComponent(jLabel2)
-                                .addComponent(jLabel7))
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(jPanel1Layout.createSequentialGroup()
-                                    .addComponent(userLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jLabel3)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(timeLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGroup(jPanel1Layout.createSequentialGroup()
-                                    .addComponent(dayLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jLabel5)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                    .addComponent(textFieldCar, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addComponent(serviceLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(31, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(216, 216, 216))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(dayLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(serviceLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(timeLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 67, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -198,32 +124,25 @@ public class PopInBooking extends javax.swing.JDialog {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(dayLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(timeLabel1)
-                    .addComponent(jLabel7)
-                    .addComponent(userLabel))
+                    .addComponent(timeLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(serviceLabel))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel5)
-                            .addComponent(textFieldCar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel4)
-                            .addComponent(serviceLabel)))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel2)
-                        .addComponent(dayLabel)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel6)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(backBtn)
-                    .addComponent(okBtn))
-                .addGap(25, 25, 25))
+                .addContainerGap(79, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -240,44 +159,9 @@ public class PopInBooking extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void backBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_backBtnMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_backBtnMouseClicked
-
-    private void okBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_okBtnMouseClicked
-      for(String[] data : users){
-        if(data[1].trim().equalsIgnoreCase(name.trim())){
-            System.out.println("User found: " + String.join(", ", data));
-            userLabel.setText(data[1]);
-
-            userId = data[0];
-            System.out.println("UserID: " + userId);
-
-            // ดึงข้อมูลจาก GUI
-            String service = serviceLabel.getText().trim();
-            String date = dayLabel.getText().trim();
-            String time = timeLabel1.getText().trim();
-            String note = textDetail.getText().trim();
-            String carReg = textFieldCar.getText().trim();
-            String status = "process"; // หรือค่าอื่นตามต้องการ
-
-            // สร้าง array ตามลำดับที่ต้องการ
-            String[] userData = {userId, data[1], service, date, time, note, carReg, status};
-
-            // เขียนข้อมูลต่อจาก CSV เดิม
-            saveBooking(userData); // ต้องสร้างฟังก์ชัน appendCSV ใน CSVHandler
-            System.out.println("Booking saved: " + String.join(", ", userData));
-
-            break;
-        }
-    }
-      dispose(); 
-    }//GEN-LAST:event_okBtnMouseClicked
-
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton backBtn;
     private javax.swing.JLabel dayLabel;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -285,14 +169,11 @@ public class PopInBooking extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JButton okBtn;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField2;
     private javax.swing.JLabel serviceLabel;
-    private javax.swing.JTextArea textDetail;
-    private javax.swing.JTextField textFieldCar;
     private javax.swing.JLabel timeLabel1;
-    private javax.swing.JLabel userLabel;
     // End of variables declaration//GEN-END:variables
 }
