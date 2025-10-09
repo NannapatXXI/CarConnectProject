24181fe41258cd8da14cb63c78bf95f63969fbf6
XXI <152409931+NannapatXXI@users.noreply.github.com>
2025-10-09 20:02:11 +0700

    Add files via upload

--- src/main/java/com/mycompany/carservice/gui/PopSuccess.java
+++ src/main/java/com/mycompany/carservice/gui/PopSuccess.java
@@ -51,7 +51,7 @@ public class PopSuccess extends javax.swing.JDialog  {
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Succeed");
+        jLabel1.setText("Success");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
